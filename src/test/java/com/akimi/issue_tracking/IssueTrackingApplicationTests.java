db09dc7d30791e29cb85a6fd6beb789178fd7e94
Milan Vlaski <milan.vlaski321@gmail.com>
2024-10-14 17:47:03 +0200

    initial commitr

---
+++ src/test/java/com/akimi/issue_tracking/IssueTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akimi.issue_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
