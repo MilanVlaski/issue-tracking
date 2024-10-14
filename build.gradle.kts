fd478344cacf66b61b186188f64ac62464a91e2c
milan.vlaski <milan.vlaski@nelkinda.com>
2024-10-14 20:23:01 +0200

    feat: got hello world in thymeleaf

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,8 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.cucumber:cucumber-java:7.20.1")
