b8300f9a2a1d3b49cd4f042dfbafab3b38a603a0
milan.vlaski <milan.vlaski@nelkinda.com>
2024-10-26 10:14:36 +0200

    fix: added sql that preloads applications

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO APLIKACIJA (NAZIV_APP, VERZIJA, OPIS, GODINA_IZDAVANJA, LOGO_URL) VALUES
+('Dev Dreams', '1.2', 'Develop web apps faster!', '2020-01-01', 'https://st3.depositphotos.com/43745012/44906/i/450/depositphotos_449066958-stock-photo-financial-accounting-logo-financial-logo.jpg'),
+('Code Sprint', '2.0', 'Speed up your coding projects!', '2021-05-15', 'https://png.pngtree.com/png-vector/20220509/ourmid/pngtree-company-logo-design-trademark-design-creative-logo-png-image_4569380.png'),
+('Design Studio', '3.5', 'Create stunning designs easily!', '2019-08-23', 'https://st5.depositphotos.com/1029305/68399/i/380/depositphotos_683998616-stock-photo-gold-abstract-hexagon-shaped-optical.jpg'),
+('TaskMaster', '4.1', 'Manage your tasks efficiently!', '2022-03-10', 'https://static8.depositphotos.com/1378583/1010/i/380/depositphotos_10106840-stock-photo-face-logo.jpg'),
+('Insight Pro', '1.8', 'Get deep insights from your data!', '2020-11-05', 'https://www.shutterstock.com/image-vector/circle-line-simple-design-logo-600nw-2174926871.jpg'),
+('Market Pulse', '2.9', 'Track market trends in real-time!', '2018-07-30', 'https://www.logodesign.net/logo/line-art-buildings-in-swoosh-1273ld.png?size=2');
