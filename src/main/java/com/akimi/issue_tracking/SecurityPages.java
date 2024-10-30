25922c7688d15cddc2360da87045a889819b0e50
milan.vlaski <milan.vlaski@nelkinda.com>
2024-10-30 14:51:31 +0100

    refactor: renamed controller with login/register pages to SecurityPages

--- src/main/java/com/akimi/issue_tracking/SecurityPages.java
+++ src/main/java/com/akimi/issue_tracking/SecurityPages.java
@@ -1,22 +1,13 @@
 package com.akimi.issue_tracking;
 
-import com.akimi.issue_tracking.entities.User;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import lombok.Data;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
 
 @Controller
-public class RegisterEndpoint {
+public class SecurityPages {
 
 
     @PersistenceContext
