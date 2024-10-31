38384c5dbcb4154f49b10bb91762c088dafd7716
milan.vlaski <milan.vlaski@nelkinda.com>
2024-10-31 16:49:18 +0100

    test: added test that checks that register doesn't work

--- src/test/java/com/akimi/issue_tracking/RegistrationTest.java
+++ src/test/java/com/akimi/issue_tracking/RegistrationTest.java
@@ -3,7 +3,6 @@ package com.akimi.issue_tracking;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -26,22 +25,37 @@ public class RegistrationTest {
     @BeforeEach
     public void setUp() {
         driver = new ChromeDriver(
-            new ChromeOptions().addArguments("--headless")
+                new ChromeOptions().addArguments("--headless")
         );
     }
 
     @Test
-    void testRegistration() {
+    void login_with_credentials_works_if_same_as_the_ones_registered_with() {
+        var email = "john@doe.com";
+        var password = "somePassword";
+        registerWithEmailAndPassword(email, password);
+        login(email, password);
+        assertEquals("http://localhost:" + port + "/", driver.getCurrentUrl());
+    }
+
+    @Test
+    void login_with_credentials_does_not_work_if_different_than_the_ones_registered_with() {
+        var email = "john@doe.com";
+        registerWithEmailAndPassword(email, "somePassword");
+        login(email, "different-pass");
+        assertEquals("http://localhost:" + port + "/login?error",
+                driver.getCurrentUrl());
+    }
+
+    private void registerWithEmailAndPassword(String email, String registeredPassword) {
         driver.get("http://localhost:" + port + "/login");
         // go to register, since we don't have an account
         new WebDriverWait(driver, Duration.ofMillis(500))
                 .until(ExpectedConditions.elementToBeClickable(By.cssSelector("[href=\"/register\"]"))).click();
         // enter user details
         driver.findElement(By.name("name")).sendKeys("John Doe");
-        var email = "john@doe.com";
-        var password = "somepassword123";
         driver.findElement(By.name("email")).sendKeys(email);
-        driver.findElement(By.name("password")).sendKeys(password);
+        driver.findElement(By.name("password")).sendKeys(registeredPassword);
         // optional
         driver.findElement(By.name("birthYear")).sendKeys("2020-10-10");
         driver.findElement(By.name("phoneNumber")).sendKeys("387231231");
@@ -49,17 +63,16 @@ public class RegistrationTest {
         location.sendKeys("New York");
 
         location.submit();
+    }
 
-        // login
+    void login(String email, String password) {
         driver.findElement(By.name("email")).sendKeys(email);
         var pass = driver.findElement(By.name("password"));
         pass.sendKeys(password);
         pass.submit();
-        // redirects us to index
-        assertEquals("http://localhost:" + port + "/", driver.getCurrentUrl());
     }
 
-        @AfterEach
+    @AfterEach
     public void tearDown() {
         driver.quit();
     }
