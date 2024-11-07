bff3136486cfa02fbfb06eef9cc7b330959b5dff
milan.vlaski <milan.vlaski@nelkinda.com>
2024-11-07 19:04:50 +0100

    fix: finding only the apps the user has purchased, for repporting a problem

--- src/main/java/com/akimi/issue_tracking/ProblemPages.java
+++ src/main/java/com/akimi/issue_tracking/ProblemPages.java
@@ -1,11 +1,15 @@
 package com.akimi.issue_tracking;
 
+import com.akimi.issue_tracking.entities.Application;
+import com.akimi.issue_tracking.entities.User;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 @Controller
 public class ProblemPages {
@@ -15,8 +19,17 @@ public class ProblemPages {
 
     @GetMapping("/reportProblem")
     public String reportProblem(Model model) {
-        var apps = em.createQuery("select a from Application a")
-                .getResultList();
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        var user = em.createQuery("select u from User u where u.email = :email", User.class)
+                     .setParameter("email", email)
+                     .getSingleResult();
+        var purchases = user.getPurchases().iterator();
+        var apps = new ArrayList<Application>();
+
+        while (purchases.hasNext()) {
+            apps.add(purchases.next().getApplication());
+        }
+
         model.addAttribute("apps", apps);
         return "reportProblem";
     }
