44ca70f8fe4ec948199f824cf70dbff757172630
milan.vlaski <milan.vlaski@nelkinda.com>
2024-11-07 18:28:50 +0100

    fix: fix tests because I chose email as a way to find user, not name

--- src/test/java/com/akimi/issue_tracking/PurchaseTest.java
+++ src/test/java/com/akimi/issue_tracking/PurchaseTest.java
@@ -27,33 +27,16 @@ public class PurchaseTest {
 
     @Test
     public void testPurchase() {
-        var username = "Jane Doe";
         var appName = "Best app";
         var supportId = "BEZ_POD"; // this is in the database by default
 
-        var user = new User().setName(username).setEmail("m@v.com");
+        var email = "m@v.com";
+        var user = new User().setName("Jane Doe").setEmail(email);
         var app = new Application().setName(appName).setVersion("1.1");
         var userId = tem.persistAndGetId(user);
         var appId = tem.persistAndGetId(app);
 
-        purchasingService.purchaseApp(supportId, appId.toString(), username);
-
-        assertEquals(appName, tem.find(User.class, userId).getPurchases().iterator().next()
-                                 .getApplication().getName());
-    }
-
-    @Test
-    public void testPurchase2() {
-        var username = "Jane Doe";
-        var appName = "Best app";
-        var supportId = "BEZ_POD"; // this is in the database by default
-
-        var user = new User().setName(username).setEmail("m@v.com");
-        var app = new Application().setName(appName).setVersion("1.1");
-        var userId = tem.persistAndGetId(user);
-        var appId = tem.persistAndGetId(app);
-
-        purchasingService.purchaseApp(supportId, appId.toString(), username);
+        purchasingService.purchaseApp(supportId, appId.toString(), email);
 
         assertEquals(appName, tem.find(User.class, userId).getPurchases().iterator().next()
                                  .getApplication().getName());
