5fa7a65316a10fd4a91c5c4a5f4b5458d8e91ee8
milan.vlaski <milan.vlaski@nelkinda.com>
2024-11-28 17:48:53 +0100

    refactor: extract common query function

--- src/main/java/com/akimi/issue_tracking/security/CurrentUser.java
+++ src/main/java/com/akimi/issue_tracking/security/CurrentUser.java
@@ -4,6 +4,7 @@ import com.akimi.issue_tracking.application.User;
 import com.akimi.issue_tracking.problem.engineer.Engineer;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -17,16 +18,20 @@ public class CurrentUser {
     @Transactional
     public User currentUser() {
         String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        return em.createQuery("select u from User u where u.email = :email", User.class)
-                 .setParameter("email", email)
-                 .getSingleResult();
+        var query = em.createQuery("select u from User u where u.email = :email", User.class);
+        return userWithEmail(query, email);
     }
 
     @Transactional
     public Engineer currentEngineer() {
         String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        return em.createQuery("select e from Engineer e where e.email = :email", Engineer.class)
-                 .setParameter("email", email)
-                 .getSingleResult();
+        var query = em.createQuery("select e from Engineer e where e.email = :email", Engineer.class);
+        return userWithEmail(query, email);
+    }
+
+    private <T> T userWithEmail(TypedQuery<T> query, String email) {
+        return query
+                .setParameter("email", email)
+                .getSingleResult();
     }
 }
