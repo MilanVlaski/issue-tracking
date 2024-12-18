90775b1625f7aa73e74c1ad8c9b133826ddbf263
milan.vlaski <milan.vlaski@nelkinda.com>
2024-12-18 18:52:13 +0100

    fix: engineer page shows up properly

--- src/main/java/com/akimi/issue_tracking/security/SecurityPages.java
+++ src/main/java/com/akimi/issue_tracking/security/SecurityPages.java
@@ -43,7 +43,7 @@ public class SecurityPages {
 
     @GetMapping("/engineer/register")
     public String engineerRegister() {
-        return "engineerRegister";
+        return "registerEngineer";
     }
 
     @PostMapping("/engineer/register")
