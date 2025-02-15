28530a24b57e60730742a5937da99270d8ba2070
Milan Vlaski <milan.vlaski321@gmail.com>
2025-02-15 08:27:30 +0100

    refactor: use record instead of lombok

--- src/main/java/com/akimi/issue_tracking/application/purchase/Support.java
+++ src/main/java/com/akimi/issue_tracking/application/purchase/Support.java
@@ -1,10 +1,4 @@
 package com.akimi.issue_tracking.application.purchase;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class Support {
-    private String support;
+public record Support(String support) {
 }
